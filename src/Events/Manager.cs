1841a6f941337b296bb1f84509c7cff28a5557ee
Muhv <muxins@vk.com>
2025-12-24 10:23:56 +0300

    Возвращены ванильные ивенты, приведение в соответствие со сценарием итогов части из них.

--- src/Events/Manager.cs
+++ src/Events/Manager.cs
@@ -14,7 +14,7 @@ namespace AugustDaysMod.Events
     [HarmonyPatch(typeof(EventManager), "OrganizeEvents")]
     public static class PatchOrganizeEventsReplace
     {
-        static bool Prefix(EventManager __instance)
+        static void Postfix(EventManager __instance)
         {
             var eventsField = AccessTools.Field(typeof(EventManager), "events");
             var crisisField = AccessTools.Field(typeof(EventManager), "crisisEvents");
@@ -22,15 +22,24 @@ namespace AugustDaysMod.Events
             var eventsDict = (Dictionary<int, AbstractEvent>)eventsField.GetValue(__instance);
             var crisisDict = (Dictionary<int, AbstractEvent>)crisisField.GetValue(__instance);
 
-            DateTime startDate = new CoupBegin().EventInfo.startDate; 
-            foreach (var eventData in eventsDict)
+            DateTime startDate = new CoupBegin().EventInfo.startDate;
+            for (int i = 0; i < eventsDict.Count; i++)
             {
-                if (eventData.Value.EventInfo.endDate < startDate)
+                if (eventsDict.TryGetValue(i, out var eventData))
                 {
-                    eventsDict.Remove(eventData.Key);
+                    if (eventData.EventInfo.endDate < startDate || 
+                        (eventData.EventInfo.startDate > new DateTime(1985, 3, 11) && eventData.EventInfo.startDate < startDate))
+                    {
+                        eventsDict.Remove(eventData.Id);
+                    }
                 }
             }
-            
+
+            //Ивент на антиалкогольную кампанию по решению
+            eventsDict.Add(5, new Backend.Gamedesign.EventSystem.Events.GameEvents.Event5());
+            //Смещение даты начала ивента Невада-Семипалатинск
+            eventsDict[290].EventInfo.startDate = new DateTime(1991, 8, 29);
+
             // Загружаем в словарь кризисных событий, потому что обычные проверяются только в определенные дни, а нам нужно каждый день
             crisisDict.Add(Negotiations.ID, new Negotiations());
             crisisDict.Add(CoupBegin.ID, new CoupBegin());
@@ -42,7 +51,7 @@ namespace AugustDaysMod.Events
             crisisDict.Add(Foros.ID, new Foros());
             crisisDict.Add(Final.ID, new Final());
 
-            return false;
+            return;
         }
     }
 }
